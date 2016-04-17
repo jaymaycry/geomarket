038b131a67fb2d6c04033c3ec86041bc1db06826
Jay Péclard <peclajea@students.zhaw.ch>
2016-04-17 23:52:08 +0200

    server unittests updated

--- server/api/offer/index.js
+++ server/api/offer/index.js
@@ -6,11 +6,11 @@ var router = express.Router();
 
 router.get('/', controller.index);
 router.get('/:id', controller.show);
-router.post('/', auth.isAuthenticated, controller.create);
-router.put('/:id', auth.isAuthenticated, controller.update);
-router.patch('/:id', auth.isAuthenticated, controller.update);
-router.delete('/:id', auth.isAuthenticated, controller.destroy);
+router.post('/', auth.isAuthenticated(), controller.create);
+router.put('/:id', auth.isAuthenticated(), controller.update);
+router.patch('/:id', auth.isAuthenticated(), controller.update);
+router.delete('/:id', auth.isAuthenticated(), controller.destroy);
 // Comment controllers
-router.put('/:id/comment/', auth.isAuthenticated, controller.commentController);
+router.put('/:id/comment', auth.isAuthenticated(), controller.commentController);
 
 module.exports = router;
