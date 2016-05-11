f3e9e5016e64d8b85d55ed2efc4bcd38716b32a1
Jay Péclard <peclajea@students.zhaw.ch>
2016-05-12 00:02:19 +0200

    improved detailOffer view, added support for timeago

--- client/app/detailOffer/detailOffer.controller.js
+++ client/app/detailOffer/detailOffer.controller.js
@@ -2,32 +2,30 @@
 (function () {
 
     class DetailOfferComponent {
-        constructor($state, $http, Offer, $stateParams, Auth) {
-            this.offer;
-            this.options = {};
+        constructor($state, $stateParams, Offer, Auth) {
             this.$state = $state;
-            this.userMap;
-            this.$http = $http;
+            this.$stateParams = $stateParams;
             this.Offer = Offer;
-            this.user;
-            this.Auth = Auth;
             this.isLoggedIn = Auth.isLoggedIn;
             this.getCurrentUser = Auth.getCurrentUser;
+            this.offer;
+            this.offerMap;
             this.owner;
-            this.$stateParams = $stateParams;
+            this.options = {};
+            this.map;
+           
         }
         $onInit() {
             this.owner = false;
-            //this.Offer = this.$state.params.obj.offerObj;
-            var _this = this;
 
-            _this.offer = this.Offer.get({ id: this.$stateParams.id }, function (e) {
-                _this.options.zoom = 13;
-                _this.options.mapTypeId = google.maps.MapTypeId.ROADMAP;
-                _this.userMap = new google.maps.Map(map, _this.options);
-                _this.setMarker(_this.offer);
-                _this.offer.endDate = new Date(_this.offer.endDate);
-                _this.isOwner();
+            this.offer = this.Offer.get({ id: this.$stateParams.id }, () => {
+                this.offer.endDate = new Date(this.offer.endDate);
+                this.options = {
+                    zoom: 15,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+                this.offerMap = new google.maps.Map(map, this.options);
+                this.setMarker(this.offer);
             });
 
         }
@@ -38,53 +36,15 @@
                 map: this.userMap,
                 animation: google.maps.Animation.DROP,
                 position: new google.maps.LatLng(offer.loc[1], offer.loc[0]),
-                visible: true
+                visible: true,
+                icon: '/assets/icons/icon.png'
             });
             var bounds = new google.maps.LatLngBounds(marker.position);
-            this.userMap.fitBounds(bounds);
+            this.offerMap.fitBounds(bounds);
         }
+        
         goBack() {
             this.$state.go("main");
-        }
-
-
-        isOwner() {
-            var _this = this;
-            _this.user = _this.getCurrentUser();
-            if (_this.user == 'undefined') {
-                return false;
-            }
-            else if (_this.owner) {
-                return true;
-            }
-            else {
-                if (_this.offer._creator == _this.user._id) {
-                    _this.owner = true;
-                    return true;
-                }
-                else {
-                    _this.owner = false;
-                    return false;
-                }
-            }
-
-        }
-
-        createComment() {
-            var _this = this;
-            _this.user = _this.getCurrentUser();
-            if (_this.user == 'undefined') {
-                _this.$state.go("login");
-            }
-            else {
-                _this.$state.go("createComment", {
-                    obj: {
-                        id: _this.offer._id,
-                        offerObj: _this.Offer
-                    }
-                });
-            }
-
         }
 
         deleteOffer() {
