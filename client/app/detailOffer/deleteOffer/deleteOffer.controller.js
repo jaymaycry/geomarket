7038b1fc6bf3ea1ff4d56aad84052ed806d8f67c
waldvmar <waldvmar@students.zhaw.ch>
2016-05-15 18:44:14 +0200

    added jsdoc for delete and sell offer

--- client/app/detailOffer/deleteOffer/deleteOffer.controller.js
+++ client/app/detailOffer/deleteOffer/deleteOffer.controller.js
@@ -1,11 +1,23 @@
 'use strict';
-
+    /**
+        * @description Represents The delete offer modal windows.
+        * @constructor
+        * @param {object} $scope - The $scope object.
+        * @param {$uibModalInstance} $uibModalInstance - The $uibModalInstance is the modal instance.
+        */
 angular.module('geomarketApp')
   .controller('DeleteOfferController', function ($scope, $uibModalInstance) {
+
+      /**
+         * @description The deletion of the offer has been confirmed. 
+         */
       $scope.ok = function () {
           $uibModalInstance.close(false);
       };
 
+      /**
+         * @description The deletion offer modal has been canceled. 
+         */
       $scope.cancel = function () {
           $uibModalInstance.dismiss('cancel');
       };
